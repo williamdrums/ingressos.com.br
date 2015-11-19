294d99d39c74c2a39e18e62b5569871244355061
Lucas <lucameiokilo@gmail.com>
2015-11-19 20:38:56 -0200

    salvando usuario

---
+++ TrabalhoGerenciaTati/cadastroUsuario.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cadastroUsuario.aspx.cs" Inherits="TrabalhoGerenciaTati.cadastroUsuario" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+         <asp:TextBox ID="TxtLogin" runat="server" Text="Login"></asp:TextBox>
+         <asp:TextBox ID="TxtSenha" runat="server" Text="Senha" TextMode="MultiLine" />
+         <asp:Button ID="btnSalvar" runat="server" Text="Salvar" OnClick="btSalvar" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ TrabalhoGerenciaTati/cadastroUsuario.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace TrabalhoGerenciaTati
+{
+    public partial class cadastroUsuario : System.Web.UI.Page
+    {
+        BancoTrabEntities3 conexao = new BancoTrabEntities3();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btSalvar(Object sender, EventArgs e) {
+            Usuario usuario = new Usuario();
+
+            usuario.usuario1 = TxtLogin.Text;
+            usuario.senha = TxtSenha.Text;
+
+            conexao.AddToUsuario(usuario);
+            conexao.SaveChanges();
+
+
+        }
+    }
+}
